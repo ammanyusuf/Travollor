af28a73fae424a6294ee15a594ebef670cacf538
ammanyusuf <amman.yusuf@ucalgary.ca>
2020-04-08 20:57:53 -0600

    Initial Commit: Template code added from Abeds TempProject

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ProjectTemp.Controllers
+{
+    [Route("api/ValuesController")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/ValuesController/GetValues
+        [HttpGet]
+        [Route("GetValues")]
+        public ActionResult<IEnumerable<string>> GetValues()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/ValuesController/GetValuesById?id=5
+        [HttpGet]
+        [Route("GetValuesById")]
+        public ActionResult<IEnumerable<string>> GetValuesById(int id)
+        {
+            return new string[] { "value1" };
+        }
+
+        // PUT api/ValuesController/InsertEmployee
+        [HttpPut]
+        [Route("InsertEmployee")]
+        public void InsertEmployee([FromBody] JObject emp)
+        {
+            string empName = (string)emp["empName"];
+            string empBdate = (string)emp["empBdate"];
+        }
+
+        // PUT api/ValuesController/UpdateEmployee
+        [HttpPost]
+        [Route("UpdateEmployee")]
+        public void UpdateEmployee([FromBody] JObject emp)
+        {
+            int empId = (int)emp["empId"];
+            string empName = (string)emp["empName"];
+            string empBdate = (string)emp["empBdate"];
+        }
+
+
+    }
+}
---
+++ Helpers/DatabaseModel.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Web;
+
+namespace ProjectTemp.Helpers
+{
+    public class DatabaseModel
+    {
+
+        #region Query Methods
+
+        public SqlConnection GetSQLConnection(string connectionstring)
+        {
+            if (connectionstring == null)
+                return null;
+            return new SqlConnection(connectionstring);
+        }
+
+        public string Get_PuBConnectionString()
+        {
+            try
+            {
+                return "Add your connection String";
+            }
+            catch { return null; }
+        }
+
+        public SqlConnection GetSQLConnection()
+        {
+            if (Get_PuBConnectionString() == null)
+                return null;
+            return new SqlConnection(Get_PuBConnectionString());
+        }
+
+        /// <summary>
+        /// This method is responisble to to execute a query in your RDBMS and return for you an output value. 
+        /// For instance, in some cases when you insert a new records you need to return the id of that record to do other actions
+        /// </summary>
+        /// <returns></returns>
+
+        public int Execute_Non_Query_Store_Procedure(string procedureName, SqlParameter[] parameters, string returnValue)
+        {
+            if (GetSQLConnection() == null)
+                return -2;
+
+            int successfulQuery = -2;
+            SqlCommand sqlCommand = new SqlCommand(procedureName, GetSQLConnection());
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                sqlCommand.Parameters.AddRange(parameters);
+                sqlCommand.Connection.Open();
+                successfulQuery = sqlCommand.ExecuteNonQuery();
+                successfulQuery = (int)sqlCommand.Parameters["@" + returnValue].Value;
+
+            }
+            catch (Exception ex)
+            {
+                string s = ex.Message;
+            }
+
+            if (sqlCommand.Connection != null && sqlCommand.Connection.State == ConnectionState.Open)
+                sqlCommand.Connection.Close();
+
+            return successfulQuery;
+        }
+
+
+        /// <summary>
+        /// This method is responisble to to execute a query in your RDBMS and return for you if it was successult executed. Minay used for insert,update, and delete
+        /// </summary>
+        /// <returns></returns>
+        public int Execute_Non_Query_Store_Procedure(string procedureName, SqlParameter[] parameters)
+        {
+            if (GetSQLConnection() == null)
+                return -1;
+
+            int successfulQuery = 1;
+            SqlCommand sqlCommand = new SqlCommand(procedureName, GetSQLConnection());
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                sqlCommand.Parameters.AddRange(parameters);
+                sqlCommand.Connection.Open();
+                successfulQuery = sqlCommand.ExecuteNonQuery();
+                // successfulQuery =1
+
+            }
+            catch (Exception ex)
+            {
+                string s = ex.Message;
+                successfulQuery = -2;
+            }
+
+            if (sqlCommand.Connection != null && sqlCommand.Connection.State == ConnectionState.Open)
+                sqlCommand.Connection.Close();
+
+            return successfulQuery;
+        }
+
+
+        /// <summary>
+        /// This method is responisble to to execute to rertieve data from your RDBSM by executing a stored procedure. Mainly used when using one select statment
+        /// </summary>
+        /// <returns></returns>
+        public DataTable Execute_Data_Query_Store_Procedure(string procedureName, SqlParameter[] parameters)
+        {
+            if (GetSQLConnection() == null)
+                return null;
+
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(procedureName, GetSQLConnection());
+            sqlAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                sqlAdapter.SelectCommand.Parameters.AddRange(parameters);
+                sqlAdapter.SelectCommand.Connection.Open();
+                sqlAdapter.Fill(dataTable);
+            }
+            catch (Exception er)
+            {
+                string ee = er.ToString();
+                dataTable = null;
+            }
+
+            if (sqlAdapter.SelectCommand.Connection != null && sqlAdapter.SelectCommand.Connection.State == ConnectionState.Open)
+                sqlAdapter.SelectCommand.Connection.Close();
+
+            return dataTable;
+        }
+
+        /// <summary>
+        /// This method is responisble to to execute to rertieve data from your RDBSM by executing a stored procedure. Mainly used when more than one table is being returned.
+        /// </summary>
+        /// <returns></returns>
+        /// 
+
+        public DataSet Execute_Data_Dataset_Store_Procedure(string procedureName, SqlParameter[] parameters)
+        {
+            if (GetSQLConnection() == null)
+                return null;
+
+            DataSet dataset = new DataSet();
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(procedureName, GetSQLConnection());
+            sqlAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                sqlAdapter.SelectCommand.Parameters.AddRange(parameters);
+                sqlAdapter.SelectCommand.Connection.Open();
+                sqlAdapter.Fill(dataset);
+            }
+            catch (Exception er)
+            {
+                string ee = er.ToString();
+                dataset = null;
+            }
+
+            if (sqlAdapter.SelectCommand.Connection != null && sqlAdapter.SelectCommand.Connection.State == ConnectionState.Open)
+                sqlAdapter.SelectCommand.Connection.Close();
+
+            return dataset;
+        }
+
+        /// <summary>
+        /// This method check if the connection string is valid or not
+        /// </summary>
+        /// <returns></returns>
+
+        public bool CheckDatabaseConnectionString(string ConnectionString)
+        {
+            try
+            {
+
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                con.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+
+        }
+        #endregion
+
+        #region Examples
+        public int updateEmployee(int empId,string empName, DateTime embBDate,string empAddress)
+        {
+
+
+            SqlParameter[] Parameters = new SqlParameter[4]; // Specifc number of parametrs for this tored procedure. 
+            Parameters[0] = new SqlParameter("@empName", empName);//Make sure parameters name matches thenames given in your stored procedure
+            Parameters[1] = new SqlParameter("@embBDate", embBDate);
+            Parameters[2] = new SqlParameter("@empAddress", empAddress);
+            Parameters[3] = new SqlParameter("@empId", empId);
+
+            return Execute_Non_Query_Store_Procedure("SP_UpdateEmpInfo", Parameters);//Make sure procedure name matches the name given in your RDBMS
+        }
+
+
+        public int insertEmployee( string empName, DateTime embBDate, string empAddress)
+        {
+            SqlParameter[] Parameters = new SqlParameter[3];
+            Parameters[0] = new SqlParameter("@empName", empName);
+            Parameters[1] = new SqlParameter("@embBDate", embBDate);
+            Parameters[2] = new SqlParameter("@empAddress", empAddress);
+
+            Parameters[2] = new SqlParameter("@empId", SqlDbType.Int);
+            Parameters[2].Direction = ParameterDirection.Output;
+
+
+            return Execute_Non_Query_Store_Procedure("SP_InsertEmpInfo", Parameters, "empId");
+        }
+
+
+        public DataTable GetEmpsInfo()
+        {
+            SqlParameter[] Parameters = new SqlParameter[0];
+
+
+            return Execute_Data_Query_Store_Procedure("SP_GetEmpsInfo", Parameters);
+
+
+        }
+
+        #endregion
+    }
+}
