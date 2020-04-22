161e7caea4758d137c1a9827e1495d505d395350
isaguimet <38235844+isaguimet@users.noreply.github.com>
2020-04-21 23:03:39 -0600

    Delete Travel Project SQL Populate Data.sql

--- Travel Project SQL Populate Data.sql
+++
@@ -1,6 +0,0 @@
-# Populate data into travellors database:
-
-INSERT INTO `travellors`.`city` (`city_id`, `name`, `country`, `state`, `province`, `district`) VALUES ('1', 'Calgary', 'Canada', 'na', 'AB', 'SW');
-INSERT INTO `travellors`.`city` (`city_id`, `name`, `country`, `state`, `province`, `district`) VALUES ('2', 'Calgary', 'Canada', 'na', 'AB', 'NW');
-INSERT INTO `travellors`.`city` (`city_id`, `name`, `country`, `state`, `province`, `district`) VALUES ('3', 'Banff', 'Canada', 'na', 'AB', 'SE');
-INSERT INTO `travellors`.`city` (`city_id`, `name`, `country`, `state`, `province`, `district`) VALUES ('4', 'Miami', 'USA', 'Florida', 'na', 'SW');
